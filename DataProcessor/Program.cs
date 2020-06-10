fdaaf3b64cb4b8af364ae9cc12099bca7d9658cc
datoukao7 <13775280@qq.com>
2020-06-10 19:35:39 +0800

    文件监控

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -6,33 +6,95 @@ using System.Threading.Tasks;
 using static System.Console; //方便一点
 using System.IO;
 
+
 namespace DataProcessor
 {
     class Program
     {
+        //public static int WatchError { get; private set; }
+
         static void Main(string[] args)
         {
             WriteLine("--file+路径 或 --dir");
-            var command = args[0];
-            if (command == "--file")
+            var directoryToWatch = args[0];
+            if (!Directory.Exists(directoryToWatch))
             {
-                var filePath = args[1];
-                WriteLine($"你选择了单个文件： {filePath} ");
-                ProcessSingleFile(filePath);
-            }
-            else if (command == "--dir")
-            {
-                var directoryPath = args[1];
-                var fileType = args[2];
-                WriteLine($"Directory {directoryPath} selected for {fileType} files");
-                ProcessDirectory(directoryPath, fileType);
+                WriteLine($"错误：{directoryToWatch} 不存在");
             }
             else
             {
-                WriteLine("无效的命令");
+                WriteLine($"监控文件夹{directoryToWatch}的变动");
+                using (var inputFileWatcher =new FileSystemWatcher(directoryToWatch))
+                {
+                    inputFileWatcher.IncludeSubdirectories = false;
+                    inputFileWatcher.InternalBufferSize = 32768;//32kb
+                    inputFileWatcher.Filter = "*.*";
+                    inputFileWatcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite ;
+
+                    inputFileWatcher.Created += FileCreated;
+                    inputFileWatcher.Changed += FileChanged;
+                    inputFileWatcher.Deleted += FileDeleted;
+                    inputFileWatcher.Renamed += FileRenamed;
+                    inputFileWatcher.Error += WatchError;
+                    inputFileWatcher.EnableRaisingEvents = true;
+                    WriteLine("按Enter退出");
+                    ReadLine();
+
+                }
             }
-            WriteLine("输入Enter退出");
-            ReadLine();
+            //##################第一个项目#############################
+            //var command = args[0];
+            //if (command == "--file")
+            //{
+            //    var filePath = args[1];
+            //    WriteLine($"你选择了单个文件： {filePath} ");
+            //    ProcessSingleFile(filePath);
+            //}
+            //else if (command == "--dir")
+            //{
+            //    var directoryPath = args[1];
+            //    var fileType = args[2];
+            //    WriteLine($"Directory {directoryPath} selected for {fileType} files");
+            //    ProcessDirectory(directoryPath, fileType);
+            //}
+            //else
+            //{
+            //    WriteLine("无效的命令");
+            //}
+            //WriteLine("输入Enter退出");
+            //ReadLine();
+            //#########################################
+
+        }
+
+        private static void WatchError(object sender, ErrorEventArgs e)
+        {
+            //throw new NotImplementedException();
+            WriteLine($"Error: file system watching may on longer be active:{e.GetException()}");
+        }
+
+        private static void FileDeleted(object sender, FileSystemEventArgs e)
+        {
+            //throw new NotImplementedException();
+            WriteLine($"文件{e.Name}被删除，类型为：{e.ChangeType}");
+        }
+
+        private static void FileRenamed(object sender, RenamedEventArgs e)
+        {
+            WriteLine($"文件{e.OldName}重命名为{e.Name}，类型为：{e.ChangeType}");
+            //throw new NotImplementedException();
+        }
+
+        private static void FileChanged(object sender, FileSystemEventArgs e)
+        {
+            //throw new NotImplementedException();
+            WriteLine($"文件{e.Name}改变，类型为：{e.ChangeType}");
+        }
+
+        private static void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            WriteLine($"文件{e.Name}创建，类型为：{e.ChangeType}");
+            //throw new NotImplementedException();
         }
 
         private static void ProcessDirectory(string directoryPath, string fileType)
